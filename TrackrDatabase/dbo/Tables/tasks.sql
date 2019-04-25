6fb7702ac07a39df18f9db77254acfa9d4c82ce1
Dallas Johnson <dallas.johnson.dev@gmail.com>
2019-04-25 15:19:06 -0400

    Added a new database project to deploy the database as well.

---
+++ TrackrDatabase/dbo/Tables/tasks.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tasks] (
+    [task_id]          INT            NOT NULL,
+    [task_name]        NVARCHAR (256) NOT NULL,
+    [task_description] NVARCHAR (MAX) NULL,
+    [task_complete]    BIT            NOT NULL,
+    CONSTRAINT [PK_dbo.tasks] PRIMARY KEY CLUSTERED ([task_id] ASC)
+);
+
